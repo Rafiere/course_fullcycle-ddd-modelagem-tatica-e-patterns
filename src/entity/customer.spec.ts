0b48d0738f7eb29d64ba334c07bc1830b8c1ddce
Rafiere <rafapgbr@gmail.com>
2023-08-01 09:22:42 -0300

    feat: add customer tests

---
+++ src/entity/customer.spec.ts
@@ -0,0 +1,10 @@
+/* É recomendado criarmos os testes perto do arquivo que está sendo
+* testado. */
+
+describe("Customer unit tests", () => {
+    it("should get 1 as result", () => {
+
+        const result = 1;
+        expect(result).toBe(1);
+    })
+})
