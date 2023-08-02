3b8a6657fca56d09d0127f9a047cd3999e94c130
Rafiere <rafapgbr@gmail.com>
2023-08-02 09:32:39 -0300

    feat: add customer spec method

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -1,10 +1,66 @@
 /* É recomendado criarmos os testes perto do arquivo que está sendo
 * testado. */
 
-describe("Customer unit tests", () => {
-    it("should get 1 as result", () => {
+import Customer from "./customer";
+import Address from "./address";
 
-        const result = 1;
-        expect(result).toBe(1);
+describe("Customer unit tests", () => {
+    it("should throw error when id is empty", () => {
+
+        expect(() => {
+            new Customer("", "John");
+        }).toThrowError("ID is required");
+    });
+
+    it("should throw error when id is empty", () => {
+
+        expect(() => {
+            new Customer("123", "");
+        }).toThrowError("Name is required");
+    })
+
+    it("should change name", () => {
+
+        //Arrange
+        const customer = new Customer("123", "John");
+
+        //Act
+        customer.changeName("Jane");
+
+        //Assert
+        expect(customer.name).toBe("Jane");
+    })
+
+    it("should activate customer", () => {
+
+        //Arrange
+        const customer = new Customer("1", "Customer 1");
+        const address = new Address("Street 1", 100, "City 1", "12345678");
+        customer.address = address;
+
+        //Act
+        customer.activate();
+
+        //Assert
+        expect(customer.isActive()).toBe(true);
+    })
+
+    it("should deactivate customer", () => {
+
+        //Arrange
+        const customer = new Customer("1", "Customer 1");
+
+        //Act
+        customer.deactivate();
+
+        //Assert
+        expect(customer.isActive()).toBe(false);
+    })
+
+    it("should throw error when address is undefined when you activate a customer", () => {
+        expect(() => {
+            const customer = new Customer("1", "Customer 1");
+            customer.activate();
+        }).toThrowError("Address is required to activate the customer.");
     })
 })
