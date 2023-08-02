7868b2acaa8a7c61199ba935a5de994a2151cb59
Rafiere <rafapgbr@gmail.com>
2023-08-02 09:31:54 -0300

    feat: add product entity

---
+++ src/entity/product.ts
@@ -0,0 +1,43 @@
+export default class Product {
+
+    private id: string;
+    private name: string;
+    private price: number;
+
+    constructor(id: string, name: string, price: number) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+
+        this.validate();
+    }
+
+    validate(){
+        if(this.id.length === 0){
+            throw new Error("Product ID is required");
+        }
+        if(this.name.length === 0){
+            throw new Error("Product name is required");
+        }
+        if(this.price < 0){
+            throw new Error("Price must be greater than zero");
+        }
+    }
+
+    changeName(name: string){
+        this.name = name;
+    }
+
+    changePrice(price: number) {
+        this.price = price;
+    }
+
+    get _name(): string {
+        return this.name;
+    }
+
+    get _price(): number {
+        return this.price;
+    }
+
+}
