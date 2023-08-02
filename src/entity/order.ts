a0d6e1b8a531e9f0aefd708505292986416fdd48
Rafiere <rafapgbr@gmail.com>
2023-08-02 09:32:18 -0300

    feat: add order method

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -12,14 +12,34 @@ export default class Order {
     * "OrderItem" dentro dele. */
     _items: OrderItem[] = [];
 
+    private _total: number;
+
+    get total(): number {
+        return this._total;
+    }
+
 
     constructor(id: string, customerId: string, items: OrderItem[]) {
         this._id = id;
         this._customerId = customerId;
         this._items = items;
+        this._total = this.calculateTotalValue();
+        this.validate();
+    }
+
+    validate(){
+        if(this._id.length === 0){
+            throw new Error("Order ID is required");
+        }
+        if(this._customerId.length === 0){
+            throw new Error("Customer ID is required");
+        }
+        if(this._items.length === 0){
+            throw new Error("Order items are required");
+        }
     }
 
     calculateTotalValue(): number {
-        return this._items.reduce((total, item) => total + item._price, 0);
+        return this._items.reduce((total, item) => total + (item._price * item._quantity), 0);
     }
 }
