9c6b38a08e8e3a57f98618003649f38256b907d5
Rafiere <rafapgbr@gmail.com>
2023-08-03 10:05:23 -0300

    feat: add main method

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,11 @@ import Order from "./entity/order";
 /* O "Customer" e o "Address" fazem parte de um agregado. */
 let customer = new Customer("1", "John");
 const address = new Address("Street 1", 100, "City 1", "12345678");
-customer._address = address;
+customer.address = address;
 customer.activate();
 
 /* O "OrderItem" e o "Ordem" fazem parte de outro agregado. */
-const item1 = new OrderItem("1", "Item 01", 100);
-const item2 = new OrderItem("1", "Item 02", 15);
+const item1 = new OrderItem("1", "Item 01", "a", 2, 200);
+const item2 = new OrderItem("1", "Item 02", "b", 3, 45);
 
-const order = new Order("1", customer._id, [item1, item2]);
+const order = new Order("1", customer.id, [item1, item2]);
