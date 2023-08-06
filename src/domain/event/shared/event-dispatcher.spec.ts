efe2e2b1a5a2335b4b4f6bc4ded3b80606da2d3b
Rafiere <rafapgbr@gmail.com>
2023-08-06 18:52:14 -0300

    feat: implement "unregister" and "unregisterAll" methods tests

--- src/domain/event/shared/event-dispatcher.spec.ts
+++ src/domain/event/shared/event-dispatcher.spec.ts
@@ -15,5 +15,47 @@ describe("Domain events tests", () => {
         expect(eventDispatcher.getEventHandlers()["ProductCreatedEvent"]).toBeDefined();
 
         expect(eventDispatcher.getEventHandlers()["ProductCreatedEvent"].length).toBe(1);
+
+        expect(eventDispatcher.getEventHandlers()["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+    })
+
+    it("should unregister an event handler", () => {
+
+        /* Estamos registrando o evento e o handler. */
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+        /* Estamos verificando se o evento está registrado. */
+        expect(eventDispatcher.getEventHandlers()["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+
+        /* Estamos removendo o registro do evento. */
+        eventDispatcher.unregister("ProductCreatedEvent", eventHandler);
+
+        /* Estamos verificando se, após removermos o handler, ele será, de fato, removido, e o evento
+        * continuará lá. */
+        expect(eventDispatcher.getEventHandlers()["ProductCreatedEvent"]).toBeDefined();
+
+        expect(eventDispatcher.getEventHandlers()["ProductCreatedEvent"].length).toBe(0);
+    })
+
+    it("should unregister all event handlers", () => {
+
+            /* Estamos registrando o evento e o handler. */
+            const eventDispatcher = new EventDispatcher();
+            const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+            eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+            /* Estamos verificando se o evento está registrado. */
+            expect(eventDispatcher.getEventHandlers()["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+
+            /* Estamos removendo o registro do evento. */
+            eventDispatcher.unregisterAll();
+
+            /* Estamos verificando se, após removermos o handler, ele será, de fato, removido, e o evento
+            * continuará lá. */
+            expect(eventDispatcher.getEventHandlers()["ProductCreatedEvent"]).toBeUndefined();
     })
 })
