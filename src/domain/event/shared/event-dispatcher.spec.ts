c26927b2a4680ba54bbd301e97b29f341f340047
Rafiere <rafapgbr@gmail.com>
2023-08-06 19:17:58 -0300

    feat: add event dispatcher tests

--- src/domain/event/shared/event-dispatcher.spec.ts
+++ src/domain/event/shared/event-dispatcher.spec.ts
@@ -1,5 +1,6 @@
 import SendEmailWhenProductIsCreatedHandler from "../product/handler/send-email-when-product-is-created.handler";
 import EventDispatcher from "./event-dispatcher";
+import ProductCreatedEvent from "../product/product-created.event";
 
 describe("Domain events tests", () => {
 
@@ -57,5 +58,31 @@ describe("Domain events tests", () => {
             /* Estamos verificando se, após removermos o handler, ele será, de fato, removido, e o evento
             * continuará lá. */
             expect(eventDispatcher.getEventHandlers()["ProductCreatedEvent"]).toBeUndefined();
+    })
+
+    it("should notify all event handlers", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+        /* Estamos criando um "spy" para espionar o "eventHandler" e verificar se
+        * ele está executando o método "handle". */
+        const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+        expect(eventDispatcher.getEventHandlers()["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+
+        const productCreatedEvent = new ProductCreatedEvent({
+            name: "Product 1",
+            description: "Product 1 description",
+            price: 100
+        });
+
+        /* Quando colocamos um "notify()", o "eventHandler" deverá executar o método
+        * "handle()". */
+        eventDispatcher.notify(productCreatedEvent)
+
+        /* Estamos garantindo que o handle foi chamado. */
+        expect(spyEventHandler).toHaveBeenCalledTimes(1);
     })
 })
