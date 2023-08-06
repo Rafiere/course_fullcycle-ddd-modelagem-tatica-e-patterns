a72f55a4e882d40f74e30794efec68b90f7ecd82
Rafiere <rafapgbr@gmail.com>
2023-08-06 18:40:31 -0300

    refactor! rename attributes

--- src/domain/entity/order.ts
+++ src/domain/entity/order.ts
@@ -2,15 +2,15 @@ import {OrderItem} from "./order-item";
 
 export default class Order {
 
-    _id: string;
+    id: string;
 
     /* A entidade vai ter apenas a referência do customer, pois
     * eles estão em agregados diferentes. */
-    _customerId: string;
+    customerId: string;
 
     /* Como estamos do mesmo agregado, teremos um array de
     * "OrderItem" dentro dele. */
-    _items: OrderItem[] = [];
+    items: OrderItem[] = [];
 
     private total: number;
 
@@ -20,26 +20,26 @@ export default class Order {
 
 
     constructor(id: string, customerId: string, items: OrderItem[]) {
-        this._id = id;
-        this._customerId = customerId;
-        this._items = items;
+        this.id = id;
+        this.customerId = customerId;
+        this.items = items;
         this.total = this.calculateTotalValue();
         this.validate();
     }
 
     validate(){
-        if(this._id.length === 0){
+        if(this.id.length === 0){
             throw new Error("Order ID is required");
         }
-        if(this._customerId.length === 0){
+        if(this.customerId.length === 0){
             throw new Error("Customer ID is required");
         }
-        if(this._items.length === 0){
+        if(this.items.length === 0){
             throw new Error("Order items are required");
         }
     }
 
     calculateTotalValue(): number {
-        return this._items.reduce((total, item) => total + (item._price * item._quantity), 0);
+        return this.items.reduce((total, item) => total + (item._price * item._quantity), 0);
     }
 }
